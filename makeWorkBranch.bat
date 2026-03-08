ba252d0cf02a481942a8941eb04de0b8289b2b1f
Victor Leskin <victorleskin16@gmail.com>
2026-03-08 20:12:34 +0300

    Add default go CI yml file

--- makeWorkBranch.bat
+++ makeWorkBranch.bat
@@ -1,8 +1,8 @@
-set HW=chore/project-setup
+set BR=chore/project-setup
 
 git switch main
 pause
 
-git checkout -b %HW%
+git checkout -b %BR%
 # create join the remove and local branchs
-git branch --set-upstream-to=origin/%HW% %HW%
+git branch --set-upstream-to=origin/%BR% %BR%
