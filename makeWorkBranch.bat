61a2f715048ec638d48e07ce1c99953fe47208ee
Victor Leskin <victorleskin16@gmail.com>
2026-03-15 13:34:48 +0300

    Add white-black-lists-service branch

--- makeWorkBranch.bat
+++ makeWorkBranch.bat
@@ -1,4 +1,5 @@
 set BR=feature/rate-limiter-service
+set BR=feature/white-black-lists-service
 
 git switch main
 pause
@@ -6,3 +7,5 @@ pause
 git checkout -b %BR%
 # create join the remove and local branchs
 git branch --set-upstream-to=origin/%BR% %BR%
+
+git push -u origin feature/%BR%
