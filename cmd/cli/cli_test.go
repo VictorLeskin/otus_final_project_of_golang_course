ef434c669f7c58729aefe85189549b89c2126311
Victor Leskin <victorleskin16@gmail.com>
2026-03-28 09:28:08 +0300

    Refactor whitelist adding test functions

--- cmd/cli/cli_test.go
+++ cmd/cli/cli_test.go
@@ -100,152 +100,90 @@ func TestCLI_parseSubnetCommand(t *testing.T) {
 	}
 }
 
-func TestCLI_whitelistAdd_Success(t *testing.T) {
-	// Создаем тестовый сервер
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Проверяем путь и метод
-		assert.Equal(t, "/whitelist/add", r.URL.Path)
-		assert.Equal(t, "POST", r.Method)
+func TestCLI_whitelistAdd(t *testing.T) {
+	tests := []struct {
+		name               string
+		args               []string
+		expectedCode       int
+		expectedOutput     string
+		expectedError      string
+		ServerStatusCode   int
+		emulateServerError bool
+	}{
+		{
+			name:               "successful",
+			args:               []string{"192.168.1.0/24"},
+			expectedCode:       0,
+			expectedOutput:     "Added 192.168.1.0/24 to whitelist\n",
+			expectedError:      "",
+			ServerStatusCode:   http.StatusOK,
+			emulateServerError: false,
+		},
+		{
+			name:               "missed subnet",
+			args:               []string{},
+			expectedCode:       1,
+			expectedOutput:     "",
+			expectedError:      "Usage: cli whitelist add <subnet>",
+			ServerStatusCode:   http.StatusOK,
+			emulateServerError: false,
+		},
+		{
+			name:               "server return bad request",
+			args:               []string{"192.168.1.0/24"},
+			expectedCode:       1,
+			expectedOutput:     "",
+			expectedError:      "Server error: 400 Bad Request",
+			ServerStatusCode:   http.StatusBadRequest,
+			emulateServerError: false,
+		},
+		{
+			name:               "internal server error",
+			args:               []string{"192.168.1.0/24"},
+			expectedCode:       1,
+			expectedOutput:     "",
+			expectedError:      "Error: Post",
+			ServerStatusCode:   http.StatusInternalServerError,
+			emulateServerError: true,
+		},
+	}
 
-		// Проверяем тело запроса
-		var req map[string]string
-		err := json.NewDecoder(r.Body).Decode(&req)
-		require.NoError(t, err)
-		assert.Equal(t, "192.168.1.0/24", req["subnet"])
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Создаем тестовый сервер
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				// Проверяем путь и метод
+				assert.Equal(t, "/whitelist/add", r.URL.Path)
+				assert.Equal(t, "POST", r.Method)
 
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer server.Close()
+				// Проверяем тело запроса
+				var req map[string]string
+				err := json.NewDecoder(r.Body).Decode(&req)
+				require.NoError(t, err)
+				assert.Equal(t, "192.168.1.0/24", req["subnet"])
 
-	// Создаем CLI с аргументами
-	cli := NewCLI([]string{"192.168.1.0/24"})
-	cli.server = server.URL
-	out := &bytes.Buffer{}
-	cli.stdout = out
+				w.WriteHeader(tt.ServerStatusCode)
+			}))
+			defer server.Close()
 
-	// Вызываем функцию
-	code := cli.whitelistAdd()
+			// Создаем CLI с аргументами
+			cli := NewCLI(tt.args)
+			cli.server = server.URL
+			if tt.emulateServerError {
+				server.Close()
+			}
+			stdout := &bytes.Buffer{}
+			cli.stdout = stdout
+			stderr := &bytes.Buffer{}
+			cli.stderr = stderr
 
-	// Проверяем результат
-	assert.Equal(t, 0, code)
-	assert.Contains(t, out.String(), "Added 192.168.1.0/24 to whitelist")
-}
+			// Вызываем функцию
+			code := cli.whitelistAdd()
 
-func TestCLI_whitelistAdd_MissedSubnet(t *testing.T) {
-	// Создаем тестовый сервер
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Проверяем путь и метод
-		assert.Equal(t, "/whitelist/add", r.URL.Path)
-		assert.Equal(t, "POST", r.Method)
-
-		// Проверяем тело запроса
-		var req map[string]string
-		err := json.NewDecoder(r.Body).Decode(&req)
-		require.NoError(t, err)
-		assert.Equal(t, "192.168.1.0/24", req["subnet"])
-
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer server.Close()
-
-	// Создаем CLI с аргументами
-	cli := NewCLI([]string{})
-	cli.server = server.URL
-	out := &bytes.Buffer{}
-	cli.stderr = out
-
-	// Вызываем функцию
-	code := cli.whitelistAdd()
-
-	// Проверяем результат
-	assert.Equal(t, 1, code)
-	assert.Contains(t, out.String(), "Usage: cli whitelist add <subnet>")
-}
-
-func TestCLI_whitelistAdd_NoSuccess(t *testing.T) {
-	// Создаем тестовый сервер
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Проверяем путь и метод
-		assert.Equal(t, "/whitelist/add", r.URL.Path)
-		assert.Equal(t, "POST", r.Method)
-
-		// Проверяем тело запроса
-		var req map[string]string
-		err := json.NewDecoder(r.Body).Decode(&req)
-		require.NoError(t, err)
-		assert.Equal(t, "192.168.1.0/24", req["subnet"])
-
-		w.WriteHeader(http.StatusBadRequest)
-	}))
-	defer server.Close()
-
-	// Создаем CLI с аргументами
-	cli := NewCLI([]string{"192.168.1.0/24"})
-	cli.server = server.URL
-	out := &bytes.Buffer{}
-	cli.stderr = out
-
-	// Вызываем функцию
-	code := cli.whitelistAdd()
-
-	// Проверяем результат
-	assert.Equal(t, 1, code)
-	assert.Contains(t, out.String(), "Server error: 400 Bad Request")
-}
-
-func TestCLI_whitelistAdd_ServerError(t *testing.T) {
-	// Создаем тестовый сервер
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Проверяем путь и метод
-		assert.Equal(t, "/whitelist/add", r.URL.Path)
-		assert.Equal(t, "POST", r.Method)
-
-		// Проверяем тело запроса
-		var req map[string]string
-		err := json.NewDecoder(r.Body).Decode(&req)
-		require.NoError(t, err)
-		assert.Equal(t, "192.168.1.0/24", req["subnet"])
-
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer server.Close()
-
-	// Создаем CLI с аргументами
-	cli := NewCLI([]string{"192.168.1.0/24"})
-	cli.server = server.URL
-	server.Close()
-	out := &bytes.Buffer{}
-	cli.stderr = out
-
-	// Вызываем функцию
-	code := cli.whitelistAdd()
-
-	// Проверяем результат
-	assert.Equal(t, 1, code)
-	assert.Contains(t, out.String(), "Error: Post ")
-}
-
-func TestCLI_whitelistAdd_WithServerFlag(t *testing.T) {
-	callCount := 0
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		callCount++
-		assert.Equal(t, "/whitelist/add", r.URL.Path)
-
-		var req map[string]string
-		json.NewDecoder(r.Body).Decode(&req)
-		assert.Equal(t, "192.168.1.0/24", req["subnet"])
-
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer server.Close()
-
-	cli := NewCLI([]string{"cli", "whitelist", "add", "--server", server.URL, "192.168.1.0/24"})
-	out := &bytes.Buffer{}
-	cli.stdout = out
-
-	code := cli.Run()
-
-	assert.Equal(t, 0, code)
-	assert.Contains(t, out.String(), "Added")
-	assert.Equal(t, 1, callCount)
+			// Проверяем результат
+			assert.Equal(t, tt.expectedCode, code)
+			assert.Contains(t, stdout.String(), tt.expectedOutput)
+			assert.Contains(t, stderr.String(), tt.expectedError)
+		})
+	}
 }
